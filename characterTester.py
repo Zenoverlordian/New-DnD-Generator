07f307e0ee135976a50f143c1737a762c21a69a4
Zenoverlordian <you@example.comC:/Users/Noah/miniconda3/Scripts/activate>
2020-07-04 18:44:22 -0400

    Added testing file

---
+++ characterTester.py
@@ -0,0 +1,4 @@
+from theMegaTool import dndChar
+test = dndChar()
+test.addAllSources()
+test.setRandBackground(); test.assignRandClass(); test.setRandRace(); test.assignStats(1); test.generateName();test.addRandMulticlass(); test.viewCharacter(); test.listCharacterSources()
